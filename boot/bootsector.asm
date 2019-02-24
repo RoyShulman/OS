235648da122045e18e05f831e7477b082803c727
RoyChuChun <roy.shulman@gmail.com>
2019-02-24 22:43:11 +0200

    Bootsector that prints hello

--- boot/bootsector.asm
+++ boot/bootsector.asm
@@ -1,5 +1,19 @@
-loop:
-	jmp loop
+; Bootsector that prints a message to the screen using BIOS routine
+
+mov ah, 0x0e ; int 0x10 with ah = 0x0e -> means scrolling teletype
+
+mov al, 'H'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'O'
+int 0x10
+
+jmp $
 
 ;$$ is the start of the current position
 ;$ is the address of current position
