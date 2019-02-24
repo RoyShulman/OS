015885b92b337d449bdd56d52a8e2801cc235183
RoyChuChun <roy.shulman@gmail.com>
2019-02-24 23:01:32 +0200

    Better string printing for bootsector

--- boot/bootsector.asm
+++ boot/bootsector.asm
@@ -1,19 +1,28 @@
 ; Bootsector that prints a message to the screen using BIOS routine
 
+; Because the BIOS maps our bootsector to 0x7c00 we tell the assembler we will be mapped there so he
+; corrects the offsets
+[org 0x7c00]
+
 mov ah, 0x0e ; int 0x10 with ah = 0x0e -> means scrolling teletype
 
-mov al, 'H'
-int 0x10
-mov al, 'E'
-int 0x10
-mov al, 'L'
-int 0x10
-mov al, 'L'
-int 0x10
-mov al, 'O'
-int 0x10
+mov bx, my_string
+loop:
+	cmp [bx], dword 0
+	je endloop
+
+	mov al, [bx]
+	int 0x10
+	add bx, word 1
+
+	jne loop
+
+endloop:
 
 jmp $
+
+my_string:
+	db 'Hello there!',0
 
 ;$$ is the start of the current position
 ;$ is the address of current position
