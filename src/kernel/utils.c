24048736e240c63263d1a3af896bace8be915ffd
RoyChuChun <roy.shulman@gmail.com>
2019-03-02 20:24:05 +0200

    Added more util functions

--- src/kernel/utils.c
+++ src/kernel/utils.c
@@ -5,3 +5,56 @@ void copy_memory(char* source, char* dest, int num_bytes) {
 		*(dest + i) = *(source + i);
 	}
 }
+
+void itoa(const int num, char* str) {
+	int i = 0;
+	char is_negative = 0;
+
+	if (num == 0) {
+		str[i] = '0';
+		str[++i] = 0;
+		return;
+	}
+
+	if (num < 0) {
+		is_negative = 1;
+		num = -num;
+	}
+
+	// Process individual digits
+	while (num != 0) {
+		int dig = num % 10;
+		str[i++] = dig + '0';
+		num /= 10;
+	}
+
+	if (is_negative) {
+		str[i++] = '-';
+	}
+
+	// Add the null terminator
+	str[i] = 0;
+
+	reverse(str);
+
+	return;
+}
+
+void reverse(char* string) {
+	int len = strlen(string);
+	for (int i = 0; i < (len / 2); i++) {
+		char temp;
+		temp = string[i];
+		string[i] = string[len - i];
+		string[len -i] = temp;
+	}
+}
+
+int strlen(char* string) {
+	int len = 0;
+	int i = 0;
+	while(string[i++] != 0) {
+		len++;
+	}
+	return len;
+}
--- src/kernel/utils.h
+++ src/kernel/utils.h
@@ -11,10 +11,34 @@
 /**
  * @brief      Helper functiont to copy num_bytes from one buffer to the other
  *
- * @param      source     The source buffer to copy from
- * @param      dest       The destination buffer to copy to
- * @param[in]  num_bytes  The number of bytes to copy
+ * @param[in]      	source     The source buffer to copy from
+ * @param[out]      dest       The destination buffer to copy to
+ * @param[in]  		num_bytes  The number of bytes to copy
  */
 void copy_memory(char* source, char* dest, int num_bytes);
+
+/**
+ * @brief      Convert an int to ascii
+ *
+ * @param[in]  		num   The int to convert
+ * @param[out]      str   The buffer to write the ascii of the int to
+ */
+void itoa(int num, char* str);
+
+/**
+ * @brief     Reverse a string
+ *
+ * @param[inout]      string  The string to reverse
+ */
+void reverse(char* string);
+
+/**
+ * @brief      Calculate the length of a null terminated string
+ *
+ * @param      string  The string
+ *
+ * @return     len of the string
+ */
+int strlen(char* string);
 
 #endif
