1d6d2b9aa3297cb0bdd7a12f91ba03c207dfafd4
RoyChuChun <roy.shulman@gmail.com>
2019-06-11 07:04:27 +0300

    Added unreal mode

--- src/boot/bootsector.asm
+++ src/boot/bootsector.asm
@@ -1,42 +1,54 @@
 ; Bootsector that prints a message to the screen using BIOS routine
+section .first-boot-sector
 
 [bits 16]
 ; Because the BIOS maps our bootsector to 0x7c00 we tell the assembler we will be mapped there so he
 ; corrects the offsets
-[org 0x7c00]
-KERNEL_OFFSET equ 0x1000	; The memory offset to which we load our kernel
+; [org 0x7c00]
+extern _kernel_start	; The memory offset to which we load our kernel
+extern _stack_end		; The memory offset the stack ends
 
-mov [BOOT_DRIVE], dl 	; BIOS stores the boot drive in dl
+_boot:
+	xor ax, ax
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
+	mov [BOOT_DRIVE], dl 	; BIOS stores the boot drive in dl
 
-mov bp, 0x9000			; Setup the stack
-mov sp, bp
+	cld 					; clear the direction flag
+	mov bp, _stack_end 		; Setup the stack
+	mov sp, bp
 
-mov bx, MSG_REAL_MODE
-call print_string
+	mov bx, MSG_REAL_MODE
+	call print_string_16
+	
+	call enter_unreal_mode 	; Enter unreal mode - 32 bit address space limit but still in 16 bit mode
 
-call load_kernel
-
-call switch_to_pm ; Never return
-
-
-jmp $
-
-load_kernel:
 	mov bx, MSG_LOAD_KERNEL
-	mov ax, 0
-	mov es, ax
-	call print_string
+	call print_string_16
+	hlt
 
-	mov bx, KERNEL_OFFSET 	; Load the first 15(just to be safe) sectors excluding the boot sector
-	mov dh, 15				; from our boot drive into the address 0x1000 which is the es:KERNEL_OFFSET
-	mov dl, [BOOT_DRIVE]
-	call disk_load
+
+
+
+	jmp $
+
+; load_kernel:
+; 	mov bx, MSG_LOAD_KERNEL
+; 	mov ax, 0
+; 	mov es, ax
+; 	call print_string_16
+
+; 	mov bx, _kernel_start 	; Load the first 15(just to be safe) sectors excluding the boot sector
+; 	mov dh, 15				; from our boot drive into the address which is the es:_kernel_start
+; 	mov dl, [BOOT_DRIVE]
+; 	call disk_load
 
 	ret
 
 %include "print_string.asm"
-%include "print_hex.asm"
 %include "read_disk.asm"
+%include "print_hex.asm"
 %include "gdt.asm"
 %include "switch_to_pm.asm"
 
@@ -48,14 +60,17 @@ BEGIN_PM:
 	mov ebx, MSG_PROT_MODE
 	call print_string_pm
 
-	call KERNEL_OFFSET 		; Jump to the kernel code!
+	call _kernel_start 		; Jump to the kernel code!
 
 	jmp $
 
 BOOT_DRIVE		db 0
+CURR_ES			dw 0
+CURR_DS			dw 0
 MSG_REAL_MODE	db "Started in 16 bit real mode!", 0
 MSG_PROT_MODE	db "Successfully landed in 32 bit protected code!", 0
 MSG_LOAD_KERNEL	db "Loading kernel into memory!", 0
+
 
 ;$$ is the start of the current position
 ;$ is the address of current position
--- src/boot/switch_to_pm.asm
+++ src/boot/switch_to_pm.asm
@@ -1,15 +1,38 @@
+extern __page_table_start
+
 [bits 16]
 switch_to_pm:
+	jmp CODE_SEG:init_pm 	; Make a far jump to our 32 bit code. This forces the CPU to flush its cache of prefetched 
+							; and real mode decoded instructions. This also sets cs to be the CODE_SEG which is what
+							; we will do for all the data segment registers(far jmp is cs:ip so this is like mov cs, CODE_SEG)
+
+enter_unreal_mode:
+	push bx
+
+	mov [CURR_ES], es 		; Save the old data segments 
+	mov [CURR_DS], ds
+
 	cli 					; Switch off interrupts until we set up the protected mode interrupt vector
 	lgdt [gdt_descriptor] 	; Load the global descriptor table which defines the protected mode segments
-
+ 	
 	mov eax, cr0 	; Get the value currently in cr0
 	or eax, 0x1 	; Set the first bit 
 	mov cr0, eax	; Make the actual switch to protected mode
 
-	jmp CODE_SEG:init_pm 	; Make a far jump to our 32 bit code. This forces the CPU to flush its cache of prefetched 
-							; and real mode decoded instructions. This also sets cs to be the CODE_SEG which is what
-							; we will do for all the data segment registers(far jmp is cs:ip so this is like mov cs, CODE_SEG)
+	mov bx, DATA_SEG	; Set the segment registers to protected mode memory access
+	mov es, bx
+	mov ds, bx
+
+	; Return to 16 bit mode with larger address space
+    and al, 0xfe    ; clear protected mode bit
+    mov cr0, eax
+
+	mov es, [CURR_ES] 
+	mov ds, [CURR_DS]
+	sti
+
+	pop bx
+	ret
 
 [bits 32]
 ; Init the stack and registers once in pm
@@ -22,6 +45,7 @@ init_pm:
 	mov fs, ax
 	mov gs, ax
 
-	mov ebp, 0x90000 	; move stack position to be right at the top of the free space
+	mov ebp, __page_table_start 	; move stack position to be right at the top of the free space
+	mov esp, ebp
 
 	call BEGIN_PM		; At the end, call a well known label
